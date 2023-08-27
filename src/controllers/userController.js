bbeb2fbca7bc550ce690743fa3c57cc5bbff6dd9
watcharapol numpaya <watcharapol.num@mail.kmutt.ac.th>
2023-08-27 23:13:50 +0700

    [UPDATE] new login endpojnt

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,5 +1,8 @@
 const pool = require("../../db");
 const queries = require("../queries/queries");
+const jwt = require("jsonwebtoken");
+const bcrypt = require("bcrypt");
+const saltRounds = 10;
 
 const getUser = async (req, res) => {
   await pool.query(queries.getUser, (err, results) => {
@@ -13,9 +16,9 @@ const getUser = async (req, res) => {
 };
 
 const getUserById = async (req, res) => {
-  const userId = req.params.userId;
-  //   res.send(userId);
-  await pool.query(queries.getUserById, [userId], (err, results) => {
+  const user_id = req.params.user_id;
+  //   res.send(user_id);
+  await pool.query(queries.getUserById, [user_id], (err, results) => {
     if (err) {
       console.log(err);
     } else {
@@ -25,8 +28,8 @@ const getUserById = async (req, res) => {
 };
 
 const getFavoriteMovie = async (req, res) => {
-  const userId = req.params.userId;
-  await pool.query(queries.getFavoriteMovie, [userId], (err, results) => {
+  const user_id = req.params.user_id;
+  await pool.query(queries.getFavoriteMovie, [user_id], (err, results) => {
     if (err) {
       console.log(err);
     } else {
@@ -36,21 +39,39 @@ const getFavoriteMovie = async (req, res) => {
 };
 
 const addNewUser = async (req, res) => {
-  const username = req.body.username;
-  const password = req.body.password;
-  await pool.query(queries.addNewUser, [username, password], (err, results) => {
-    if (err) {
-      console.log(err);
-      console.log(req);
-    } else {
-      res.send("Value insert");
+  const { username, password } = req.body;
+  const encryptPassword = await bcrypt.hash(password, saltRounds);
+
+  // Check if the user already exists in the database
+  const userExistsResult = await pool.query(queries.getCheckUserExist, [
+    username,
+  ]);
+
+  const existingUserCount = userExistsResult.rows[0].count;
+  if (existingUserCount > 0) {
+    return res.status(400).send("Username already exists");
+  }
+
+  // If user doesn't exist, insert the new user
+  const insertUserQuery =
+    "INSERT INTO users (username, password) VALUES($1, $2)";
+  await pool.query(
+    insertUserQuery,
+    [username, encryptPassword],
+    (err, results) => {
+      if (err) {
+        console.log(err);
+        return res.status(500).send("Error registering user");
+      } else {
+        res.status(201).send("Register Complete");
+      }
     }
-  });
+  );
 };
 
 const removeUser = async (req, res) => {
-  const userId = req.params.userId;
-  await pool.query(queries.removeUser, [userId], (err, results) => {
+  const user_id = req.params.user_id;
+  await pool.query(queries.removeUser, [user_id], (err, results) => {
     if (err) {
       console.log(err);
     } else {
@@ -60,29 +81,29 @@ const removeUser = async (req, res) => {
 };
 
 const addFavoriteMovie = async (req, res) => {
-  const userId = req.body.userId;
+  const user_id = req.body.user_id;
   const favorite_movie = req.body.favorite_movie;
   await pool.query(
     queries.addFavoriteMovie,
-    [favorite_movie, userId],
+    [favorite_movie, user_id],
     (err, results) => {
       if (err) {
         console.log(err);
       } else {
-        res.send("insert complete");
+        res.status(200).send("insert complete");
       }
     }
   );
 };
 
 const removeFavoriteMovie = async (req, res) => {
-  const userId = req.body.userId;
+  const user_id = req.body.user_id;
   const favorite_movie = req.body.favorite_movie;
   const errors = [];
 
   for (const movieId of favorite_movie) {
     try {
-      await pool.query(queries.removeFavoriteMovie, [movieId, userId]);
+      await pool.query(queries.removeFavoriteMovie, [movieId, user_id]);
     } catch (err) {
       errors.push(err);
     }
@@ -96,20 +117,46 @@ const removeFavoriteMovie = async (req, res) => {
   }
 };
 
- 
 const getLogin = async (req, res) => {
   const username = req.body.username;
   const password = req.body.password;
-  await pool.query(queries.getLogin, [username, password], (err, results) => {
-    if (err) {
-      console.log(err);
-    } else {
-      // res.send("Login Success");
-      res.status(200).send(results.rows);
-    }
-  });
+  try {
+    await pool.query(queries.getLogin, [username], async (err, results) => {
+      if (err) {
+        res.status(500).send(err);
+      } else {
+        if (results.rows.length === 1) {
+          await bcrypt
+            .compare(password, results.rows[0].password)
+            .then(() => {
+              // Generate a JWT token
+              const user = results.rows[0];
+              const token = jwt.sign(
+                {
+                  user_id: user.user_id,
+                  username: user.username,
+                  favorite_movie: user.favorite_movie,
+                },
+                process.env.TOKEN_KEY,
+                {
+                  expiresIn: "1h",
+                }
+              );
+              res.status(200).json({ token });
+            })
+            .catch((err) => {
+              console.log(err);
+            });
+        } else {
+          res.status(401).send("Invalid credentials");
+        }
+      }
+    });
+  } catch (err) {
+    console.error(err);
+    res.status(500).send("An error occurred while processing your request.");
+  }
 };
- 
 
 module.exports = {
   getUser,
@@ -119,5 +166,5 @@ module.exports = {
   removeUser,
   addFavoriteMovie,
   removeFavoriteMovie,
-  getLogin, 
+  getLogin,
 };
--- src/queries/queries.js
+++ src/queries/queries.js
@@ -1,25 +1,28 @@
 const getUser = "SELECT * FROM users";
-const getUserById = "SELECT * FROM users WHERE userid = $1";
-const getFavoriteMovie = `SELECT favorite_movie FROM users WHERE userId = $1`;
+const getUserById = "SELECT * FROM users WHERE user_id = $1";
+const getFavoriteMovie = `SELECT favorite_movie FROM users WHERE user_id = $1`;
 const addNewUser = `INSERT INTO users (username, password) VALUES($1, $2);`;
-const removeUser = `DELETE FROM users WHERE userId = $1`;
+const removeUser = `DELETE FROM users WHERE user_id = $1`;
 const addFavoriteMovie = `
   UPDATE users
   SET favorite_movie = (
     SELECT array_agg(DISTINCT movieId)
     FROM unnest(array_cat(favorite_movie, $1::int[])) AS movieId
   )
-  WHERE userId = $2
+  WHERE user_id = $2
   RETURNING *;
 `;
 
 const removeFavoriteMovie = `
 UPDATE users
 SET favorite_movie = array_remove(favorite_movie, $1)
-WHERE userId = $2;
+WHERE user_id = $2;
 `;
 
-const getLogin =`SELECT * FROM users WHERE username = $1 AND password = $2`  ;
+const getLogin = `SELECT * FROM users WHERE username = $1`;
+const getCheckUserExist =
+  "SELECT COUNT(*) AS count FROM users WHERE username = $1";
+
 
 module.exports = {
   getUser,
@@ -30,4 +33,5 @@ module.exports = {
   addNewUser,
   removeUser,
   getLogin,
+  getCheckUserExist,
 };
