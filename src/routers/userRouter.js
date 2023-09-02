fbb26c2b85847f98e48975b2a279ccdc21716743
watcharapol numpaya <watcharapol.num@mail.kmutt.ac.th>
2023-09-03 01:41:55 +0700

    [UPDATE] check add get favorite movie to post request

--- src/routers/userRouter.js
+++ src/routers/userRouter.js
@@ -7,14 +7,14 @@ const auth = require('../middleware/auth')
 //get request 
 router.get("/", userController.getAllUser);
 router.get("/:user_id", userController.getUserById);
-router.get("/:user_id/favorite_movie", userController.getFavoriteMovie);
+ 
  
 //post request
 router.post("/register", userController.addNewUser);
 router.post("/sign-in", userController.getLogin);
 router.post("/authentication" ,auth.verifyAccessToken,userController.getAuthentication)
 router.post("/refresh_token" ,auth.verifyRefreshToken,userController.getRefreshToken)
- 
+router.post("/favorite_movie", userController.getFavoriteMovie); 
 //put request
 router.put("/add_favorite_movie", userController.addFavoriteMovie);
 router.put("/remove_favorite_movie", userController.removeFavoriteMovie);
